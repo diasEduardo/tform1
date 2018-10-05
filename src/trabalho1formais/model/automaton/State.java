344ffbb90771caf4413af4c3f7e2e27b5659ee2d
Ad Nunes Ribeiro <adnunesribeiro@gmail.com>
2018-10-05 18:10:41 -0300

    Intersection and union

--- src/trabalho1formais/model/automaton/State.java
+++ src/trabalho1formais/model/automaton/State.java
@@ -1,13 +1,36 @@
 
 package trabalho1formais.model.automaton;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author nathan
  */
 public class State {
     private String name;
-
+    private boolean marked;
+    private ArrayList<Integer> followpos;
+    
+    public State(String name) {
+        this.name = name;
+    }
+    
+    public State(ArrayList<Integer> f) {
+    	String sName = "";
+    	for(int i = 0;i<f.size();i++) {
+    		sName = sName + f.get(i);
+    	}
+    	followpos = f;
+        this.name = sName;
+        marked = false;
+    }
+    
+    
+    public boolean getMarked(){
+    	return marked;
+    }
+    
     public String getName() {
         return name;
     }
@@ -16,13 +39,18 @@ public class State {
         this.name = name;
     }
     
-    
-    public State(String name) {
-        this.name = name;
-    }
-    
     @Override
     public String toString() {
             return this.name;
     }
+
+	public void setMarked() {
+		marked = true;
+		// TODO Auto-generated method stub
+		
+	}
+
+	public ArrayList<Integer> getFollowpos() {
+		return followpos;
+	}
 }
